555570b41559287e212190c950400f1757e0b795
TahirAhmad <tahirahmad0234@gmail.com>
2024-09-02 23:44:36 +0600

    layout position fixed

--- app/surah/layout.jsx
+++ app/surah/layout.jsx
@@ -4,10 +4,12 @@ async function LeftLayout({ children }) {
 
   return (
     <div className="flex justify-between gap-4 w-full my-5">
-      <div className="w-96 hidden md:block max-h-full overflow-hidden">
+      <div className="w-96 hidden md:block max-h-full overflow-hidden h-[calc(100vh-89px)] overflow-y-auto">
         <LeftBar />
       </div>
-      <div className="w-full bg-white rounded-md">{children}</div>
+      <div className="w-full bg-white rounded-md h-[calc(100vh-89px)] overflow-y-auto">
+        {children}
+      </div>
     </div>
   );
 }
